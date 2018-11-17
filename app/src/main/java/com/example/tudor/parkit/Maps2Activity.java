c1db2108c0be42e462a28ef844af0ea5cd233e2b
TudorStadiu <36773944+TudorStadiu@users.noreply.github.com>
2018-11-17 21:09:08 +0200

    working location/multiple pinpoints

--- app/src/main/java/com/example/tudor/parkit/Maps2Activity.java
+++ app/src/main/java/com/example/tudor/parkit/Maps2Activity.java
@@ -1,9 +1,15 @@
 package com.example.tudor.parkit;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,19 +21,144 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Maps2Activity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+    private static final int REQUEST_LOCATION_PERMISSION = 123;
+    private GoogleMap mMap;
+    private String username;
+    private String password;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        enableMyLocation();
+        // Add a marker in Sydney and move the camera
+
+        getPinpoints();
+
+
+    }
+
+
+
+    private void getPinpoints() {
+        RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+        String BASEURL ="https://murmuring-journey-83684.herokuapp.com";
+        String PINPOINS ="/pinpoints";
+
+
+
+        JsonArrayRequest objectRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                BASEURL+PINPOINS,
+                null,
+
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try{
+                            // Loop through the array elements
+                            for(int i=0;i<response.length();i++){
+                                // Get current json object
+                                JSONObject pinpoint = response.getJSONObject(i);
+                                LatLng pinpointLatLng = new LatLng(pinpoint.getDouble("latitude"), pinpoint.getDouble("longitude"));
+                                mMap.addMarker(new MarkerOptions().position(pinpointLatLng).title(pinpoint.getString("title")));
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Eroare::::",error.toString());
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params =  new HashMap<>();
+                String token = username+":"+password;
+                String finalToken = new String(Base64.encode(token.getBytes(),Base64.DEFAULT));
+                Log.d("finalToken", finalToken);
+                params.put("Authorization","Basic "+ finalToken);
+                //..add other headers
+                return params;
+            }
+        };
+
+        queue.add(objectRequest);
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+
+
+
+
+
+
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                    break;
+                }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps2);
 
+
         SharedPreferences prefs = getSharedPreferences("user_info", MODE_PRIVATE);
-        String first_name = prefs.getString("first_name", null);
-        String last_name = prefs.getString("first_name", null);
-        Log.d("FIRSTNAME:::",first_name);
-        Log.d("LASTNAME:::",first_name);
+        username = prefs.getString("first_name", null);
+        password = prefs.getString("last_name", null);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
