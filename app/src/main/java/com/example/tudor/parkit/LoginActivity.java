a639c02ab1170f11beabb744f8e47b2d5e77f1fc
TudorStadiu <36773944+TudorStadiu@users.noreply.github.com>
2018-11-17 16:49:20 +0200

    ceva

--- app/src/main/java/com/example/tudor/parkit/LoginActivity.java
+++ app/src/main/java/com/example/tudor/parkit/LoginActivity.java
@@ -3,18 +3,23 @@ package com.example.tudor.parkit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -40,26 +45,38 @@ public class LoginActivity extends AppCompatActivity {
                 Log.d("PASSWORD:::::",passwordText.getText().toString());
 
                 RequestQueue queue = Volley.newRequestQueue(getBaseContext());
-                String url ="https://murmuring-journey-83684.herokuapp.com/?fbclid=IwAR0Kn7uYngNDGB0aGgbS9p4N93mv_cwKSEceNIjVruyr6x2EPhSa29641cA";
-                //String BASEURL ="https://murmuring-journey-83684.herokuapp.com/?fbclid=IwAR0Kn7uYngNDGB0aGgbS9p4N93mv_cwKSEceNIjVruyr6x2EPhSa29641cA";
+                String BASEURL ="https://murmuring-journey-83684.herokuapp.com/?fbclid=IwAR0Kn7uYngNDGB0aGgbS9p4N93mv_cwKSEceNIjVruyr6x2EPhSa29641cA";
+                String PROFILE ="/profile";
+
 
 // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, BASEURL+PROFILE,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
                                 loginText.setText("Response is: "+ response);
-                                if(loginText.getText().toString().contains("works")){
-                                    Intent screen2 = new Intent(LoginActivity.this, MapsActivity.class);
-                                    startActivity(screen2);
-                                }
                             }
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         loginText.setText("That didn't work!");
+                        Log.d("Eroare::::",error.toString());
                     }
-                });
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String,String> params =  super.getHeaders();
+                        if(params==null)params = new HashMap<>();
+                        String token = emailText.getText().toString()+":"+passwordText.getText().toString();
+                        String finalToken = new String(Base64.encode(token.getBytes(),Base64.DEFAULT));
+                        Log.d("finalToken", finalToken);
+                        params.put("Authorization","Basic "+ finalToken);
+                        //..add other headers
+                        return params;
+                    }
+                };
 
 // Add the request to the RequestQueue.
                 queue.add(stringRequest);
--- app/src/main/java/com/example/tudor/parkit/MainActivity.java
+++ app/src/main/java/com/example/tudor/parkit/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.tudor.parkit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         startButton = findViewById(R.id.start_btn);
